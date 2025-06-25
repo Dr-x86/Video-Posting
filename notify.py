83ef5f44c0c03c5b1b2fe5d79073f1647a1e3a36
Dr-x86 <romeroibarradaniel@gmail.com>
2025-06-25 12:05:17 -0600

    Less is more

--- notify.py
+++ notify.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 CHAT_ID=os.getenv("PERSONAL_CHAT_ID")
 TOKEN=os.getenv("BOT_TOKEN")
-GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID')
 
 def Me(msg):
     URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"    
@@ -15,14 +14,3 @@ def Me(msg):
         "parse_mode": "Markdown"
     }
     respuesta = requests.post(URL, data=parametros)
-
-
-def Channel(IMAGE_URL,CAPTION = ''):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
-    payload = {
-        "chat_id": "@WaiFUNotSF",  # Usa '@nombre_del_canal' o 
-        "photo": IMAGE_URL,
-        "caption": CAPTION
-    }
-    response = requests.post(url, data=payload)
-    return response
